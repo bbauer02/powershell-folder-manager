f9f6433e6cd4f960344f9f3265bd74b7cab982b8
Bauer <bbauer02@gmail.com>
2022-05-17 14:04:39 +0200

    correction script POWERSHELL région / département

--- gestion_departement/script.ps1
+++ gestion_departement/script.ps1
@@ -3,23 +3,32 @@ $regions = Get-Content .\region.json -Raw | ConvertFrom-Json
 $departments = Get-Content .\departments.json -Raw | ConvertFrom-Json
 $cities = Get-Content .\cities.json -Raw | ConvertFrom-Json
 
+
 foreach( $region in $regions) {
-    $region_num = -join("[",$region.code,"]", " ", $region.slug)
-    Write-Output $region_num
+    $regionName = -join($region.code,"-", $region.slug)
+    # Write-Output $region_num
+    New-Item -Path $pwd/data -Name $regionName -ItemType "directory"
+    
     $filtered_deps = $departments | Where-Object { $_.region_code -eq $region.code  } 
     
     foreach( $department in  $filtered_deps ) {
-        $formatted_dep = -join("-->", "(",$department.code, ")",$department.slug)
-        Write-Output $formatted_dep
+        $depName = -join($department.code,"-", $department.slug)
+        New-Item -Path $pwd/data/$regionName -Name $depName -ItemType "directory"
+
+        $filtered_cities = $cities | Where-Object { $_.department_code -eq $department.code  }
+
+        foreach( $city in  $filtered_cities ) {
+            $cityName = -join($city.id,"-",$city.slug,'.txt') 
+            $cityName = $cityName -replace(" ","-")
+            New-Item -Path $pwd/data/$regionName/$depName/$cityName -ItemType "file"
+
+            $content = -join($city.zip_code, " ", $city.name, "\n", $city.gps_lat)
+
+            ADD-content -path $pwd/data/$regionName/$depName/$cityName -value $content
+        }
+
     }
 }
-
-## Création du dossier de cours parent
-#  New-Item -Path $ROOT_FOLDER -Name $cours -ItemType "directory"
-
- # On récupére la liste des dossiers et fichiers présents dans le répertoire courant
- # $childrenItems = Get-ChildItem -Path $ROOT_FOLDER
-
 
 
 
